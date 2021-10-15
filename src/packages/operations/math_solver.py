2e3fbb6a5b7f1e524ebaa64ce72b9f26bb667433
Kai Jie Chow <kj1chow@gmail.com>
2021-10-16 00:32:49 +0100

    to be continue tomorrow

--- src/packages/operations/math_solver.py
+++ src/packages/operations/math_solver.py
@@ -1,5 +1,6 @@
 import re
 import math
+import copy
 import time
 from queue import PriorityQueue
 from math_operators import OperatorFunction
@@ -36,9 +37,10 @@ class MathSolver():
         ### VALID EQUATION ###
         check_list = list(
             {**self.primary_priority, **self.secondary_priority, **self.special_constants}.keys()) + self.special_operator + self.indicator
-        index_var_dict = self.find_variables(
+        index_var_dict = self.variable_search(
             string_equation, check_list)
-        if not self.is_valid_equation(string_equation, index_var_dict):
+        unknown_var = self.is_valid_equation(string_equation, index_var_dict)
+        if not unknown_var:
             raise Exception("Not enough arguments to resolve equation.")
         if string_equation.count("(") != string_equation.count(")"):
             raise Exception("Number of brackets do not match.")
@@ -60,13 +62,14 @@ class MathSolver():
                 special_constant, str(self.special_constants[special_constant]))
         ### BRACKETS PRIORITY ###
         sub_index_dict = self.set_priority(string_equation)
-        substituted_dict = self.substitute_value(
+        orig_sub_dict = self.variable_substitution(
             string_equation, sub_index_dict)
-        for initial, sub in substituted_dict.items():
+        for initial, sub in orig_sub_dict.items():
             string_equation = string_equation.replace(initial, sub)
         ### REDUCED EQUATION ###
         try:
-            solve_special = self.get_special({}, string_equation[:-1])[1]
+            solve_special = self.special_operator_value(
+                {}, string_equation[:-1])[1]
             final_ans = float(self.simple_solver(solve_special))
         except:
             # there exists one unknown within the formula
@@ -74,15 +77,15 @@ class MathSolver():
             left_hand, right_hand = reduced_equation.split("=")
             # resolve any lower level special operator
             left_hand = self.simple_solver(
-                self.get_special({}, left_hand)[1])
+                self.special_operator_value({}, left_hand)[1])
             right_hand = self.simple_solver(
-                self.get_special({}, right_hand)[1])
-            final_ans = self.twin_solver(left_hand, right_hand)
+                self.special_operator_value({}, right_hand)[1])
+            final_ans = self.twin_solver(left_hand, right_hand, unknown_var)
         return final_ans
 
     def simple_solver(self, sub_string: str):
         sub_string = self.basic_solver(sub_string)
-        sub_string = self.resolve_low_prior(sub_string)
+        sub_string = self.complementary_solver(sub_string)
         return sub_string
 
     def basic_solver(self, sub_string: str):
@@ -136,8 +139,23 @@ class MathSolver():
                 # Accept any kind of error
                 return f"({sub_string[:-1]})"
 
-    def resolve_low_prior(self, string_equation: str):
-        string_function = string_equation.lstrip("(").rstrip(")")
+    def twin_solver(self, left_hand: str, right_hand: str, target_variable: str):
+        """Function to resolve unknown"""
+        left_hand = self.strip_trimming(left_hand)
+        right_hand = self.strip_trimming(right_hand)
+        if target_variable in right_hand:
+            final_ans = self.equation_solver(
+                right_hand, left_hand, target_variable)
+        else:
+            final_ans = self.equation_solver(
+                left_hand, right_hand, target_variable)
+        return final_ans
+
+    def equation_solver(self, var_side, value_side, target_variable):
+        return None
+
+    def complementary_solver(self, string_equation: str):
+        string_function = self.string_trimming(string_equation)
         low_prior_value = min(list(self.primary_priority.values()))
         for key, item in self.primary_priority.items():
             if item != low_prior_value:
@@ -181,7 +199,12 @@ class MathSolver():
                 special_key, special_dict[special_key], string_equation)
         return string_equation + "#"
 
-    def find_variables(self, string_equation: str, check_list: list):
+    def strip_trimming(self, string_expression: str):
+        if string_expression[0] == "(" and string_expression[-1] == ")":
+            return string_expression[1:-1]
+        return string_expression
+
+    def variable_search(self, string_equation: str, check_list: list):
         # trimming string
         string_equation = self.string_trimming(string_equation)
         index_var_dict, variable_string = {}, ""
@@ -199,20 +222,24 @@ class MathSolver():
     def is_valid_equation(self, string_equation: str, index_var_dict: dict):
         # trimming string
         string_equation = self.string_trimming(string_equation)
-        valid_counter = 0
-        for index in list(index_var_dict.keys()):
+        copy_dict = copy.deepcopy(index_var_dict)
+        for index in list(copy_dict.keys()):
             try:
-                float(index_var_dict[index])
+                float(copy_dict[index])
+                del copy_dict[index]
             except ValueError:
-                valid_counter += 1
+                if copy_dict[index] in self.defined_var_dict.keys():
+                    del copy_dict[index]
         if "=" in string_equation:
             # always one argument short
-            if valid_counter != len(self.defined_var_dict)+1:
+            if len(copy_dict) != 1:
                 return False
         else:
             # equals argument number
-            if valid_counter != len(self.defined_var_dict):
+            if len(copy_dict) != 0:
                 return False
+        if list(copy_dict.values()):
+            return list(copy_dict.values())[0]
         return True
 
     def set_priority(self, string_equation: str):
@@ -231,7 +258,7 @@ class MathSolver():
                     priority += self.secondary_priority[character]
         return sub_index_dict
 
-    def get_special(self, substituted_dict: dict, sub_expression: str):
+    def special_operator_value(self, orig_sub_dict: dict, sub_expression: str):
         # variable will not be processed due to no bracket regex rule
         primary_regex = "\)|\(|$|"
         for primary_operator in self.primary_priority:
@@ -245,34 +272,29 @@ class MathSolver():
                     if match[0]:
                         special_function = self.math_operator.sign_to_function[special_operator]
                         special_ans = str(special_function(match[0]))
-                        substituted_dict[special_operator +
-                                         match[0]] = special_ans
+                        orig_sub_dict[special_operator +
+                                      match[0]] = special_ans
                         sub_expression = sub_expression.replace(special_operator +
                                                                 match[0], special_ans)
-        return substituted_dict, sub_expression
+        return orig_sub_dict, sub_expression
 
-    def substitute_value(self, string_equation: str, sub_index_dict: dict):
+    def variable_substitution(self, string_equation: str, sub_index_dict: dict):
         # regex catching
-        substituted_dict = {}
+        orig_sub_dict = {}
         for _, value in sorted(sub_index_dict.items(), reverse=True):
             pairs = [value[2*ind:2*ind+2] for ind in range(int(len(value)/2))]
             for pair in pairs:
                 sub_expression = string_equation[pair[0]:pair[1]+1]
-                for key_sub, value_sub in substituted_dict.items():
+                for key_sub, value_sub in orig_sub_dict.items():
                     sub_expression = sub_expression.replace(
                         key_sub, value_sub)
-                substituted_dict, sub_expression = self.get_special(
-                    substituted_dict, sub_expression)
-                substituted = self.simple_solver(sub_expression[1:-1])
-                substituted_dict[sub_expression] = substituted
-        return substituted_dict
-
-    def twin_solver(self, left_hand: str, right_hand: str):
-        left_hand = left_hand.rstrip(")").lstrip("(")
-        right_hand = right_hand.rstrip(")").lstrip("(")
-        return None
+                orig_sub_dict, sub_expression = self.special_operator_value(
+                    orig_sub_dict, sub_expression)
+                final_sub = self.simple_solver(sub_expression[1:-1])
+                orig_sub_dict[sub_expression] = final_sub
+        return orig_sub_dict
 
 
 test = MathSolver(
-    "56/23*2+1+2+22*[exp^P1+123/23 + 9*3]-exp^(9) = 3*2+1")
+    "(T1/T2)^(gamma) = (P1/P2)**(gamma)", t1=5, t2=5, gamma=1.25, p2=3)
 answer = test.linear_solver()
