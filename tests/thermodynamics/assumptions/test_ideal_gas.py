62df4cfb58518bd96a1854e344e37e8c0bc38cf4
kchow <kchow@illumina.com>
2022-07-09 21:10:36 +0100

    single and stack decorators

--- tests/thermodynamics/assumptions/test_ideal_gas.py
+++ tests/thermodynamics/assumptions/test_ideal_gas.py
@@ -3,9 +3,18 @@ from solver import expression_solver
 from thermodynamics.assumptions import ideal_gas, assumptions
 
 
-@ideal_gas.IdealGas(pressure="P1", temperature="T1")
 @ideal_gas.IdealGas()
 class FakeDevice(assumptions.AbstractDevice):
+    def __init__(self, list):
+        self._list = list
+
+    def __call__(self):
+        return self._list
+
+
+@ideal_gas.IdealGas()
+@ideal_gas.IdealGas(pressure="P1")
+class StackDevice(assumptions.AbstractDevice):
     def __init__(self, list):
         self._list = list
 
@@ -19,7 +28,15 @@ class TestAssumptions:
         expected = [
             expression_solver.EquationFormat("123", "456"),
             expression_solver.EquationFormat("P*V", "n*8.3145*T"),
-            expression_solver.EquationFormat("P1*V", "n*8.3145*T1"),
+        ]
+        observed = device()
+        assert expected == observed
+
+    def test_stack_decorator(self):
+        device = StackDevice([])
+        expected = [
+            expression_solver.EquationFormat("P1*V", "n*8.3145*T"),
+            expression_solver.EquationFormat("P*V", "n*8.3145*T"),
         ]
         observed = device()
         assert expected == observed
